376efd7e47ccf51ff4ff892b0207e3d7a5db61d0
Hillay Koon <iace@foxmail.com>
2024-08-22 03:55:12 +0800

    Update chat.go
    修改模型

--- relay/chat.go
+++ relay/chat.go
@@ -82,13 +82,23 @@ func (r *relayChat) send() (err *types.OpenAIErrorWithStatusCode, done bool) {
 			return r.getUsageResponse()
 		}
 
-		err = responseStreamClient(r.c, response, r.cache, doneStr)
+		// 包装 StreamReaderInterface 以修改模型名称
+		wrappedResponse := &modelNameWrapper{
+			StreamReaderInterface: response,
+			originalModel:         r.originalModel,
+		}
+
+		err = responseStreamClient(r.c, wrappedResponse, r.cache, doneStr)
 	} else {
 		var response *types.ChatCompletionResponse
 		response, err = chatProvider.CreateChatCompletion(&r.chatRequest)
 		if err != nil {
 			return
 		}
+
+		// 修改响应中的模型名称
+		response.Model = r.originalModel
+
 		err = responseJsonClient(r.c, response)
 
 		if err == nil && response.GetContent() != "" {
@@ -103,13 +113,45 @@ func (r *relayChat) send() (err *types.OpenAIErrorWithStatusCode, done bool) {
 	return
 }
 
+// modelNameWrapper 包装 StreamReaderInterface 以修改流式响应中的模型名称
+type modelNameWrapper struct {
+	requester.StreamReaderInterface[string]
+	originalModel string
+}
+
+func (w *modelNameWrapper) Recv() (<-chan string, <-chan error) {
+	dataChan, errChan := w.StreamReaderInterface.Recv()
+	wrappedDataChan := make(chan string)
+
+	go func() {
+		defer close(wrappedDataChan)
+		for data := range dataChan {
+			// 解析 JSON
+			var jsonData map[string]interface{}
+			if err := json.Unmarshal([]byte(data), &jsonData); err == nil {
+				// 修改模型名称
+				if _, ok := jsonData["model"]; ok {
+					jsonData["model"] = w.originalModel
+					// 重新编码为 JSON
+					if modifiedData, err := json.Marshal(jsonData); err == nil {
+						data = string(modifiedData)
+					}
+				}
+			}
+			wrappedDataChan <- data
+		}
+	}()
+
+	return wrappedDataChan, errChan
+}
+
 func (r *relayChat) getUsageResponse() string {
 	if r.chatRequest.StreamOptions != nil && r.chatRequest.StreamOptions.IncludeUsage {
 		usageResponse := types.ChatCompletionStreamResponse{
 			ID:      fmt.Sprintf("chatcmpl-%s", utils.GetUUID()),
 			Object:  "chat.completion.chunk",
 			Created: utils.GetTimestamp(),
-			Model:   r.chatRequest.Model,
+			Model:   r.originalModel,
 			Choices: []types.ChatCompletionStreamChoice{},
 			Usage:   r.provider.GetUsage(),
 		}
